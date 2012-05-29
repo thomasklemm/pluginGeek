09be90f986a76cb8c6c4470f6495afae24ad9d22
Thomas Klemm <mail@tklemm.eu>
2012-05-29 15:28:09 +0200

    Category.update method to update categories in the background

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,8 +4,18 @@ class Category < ActiveRecord::Base
   def self.update
     tags = Repo.tag_counts_on(:categories)
     tags.each do |tag|
-      category = Category.find_or_initialize_by_name(tag.name)
+      category = self.find_or_initialize_by_name(tag.name)
+      
       category[:count] = tag.count
+
+      repos = Repo.tagged_with(tag).limit(4)
+
+      popular_repos = []
+      repos.each do |repo|
+        popular_repos << repo.full_name
+      end
+      category[:popular_repos] = popular_repos.join(", ")
+
       category.save
     end
   end
