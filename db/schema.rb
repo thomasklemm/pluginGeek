ff35ba35f608614ac028d2e39a4fa7574f58191b
Thomas Klemm <mail@tklemm.eu>
2012-05-29 10:39:58 +0200

    run rails g acts_as_taggable_on:migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120528091405) do
+ActiveRecord::Schema.define(:version => 20120529083940) do
 
   create_table "repos", :force => true do |t|
     t.string   "owner"
@@ -27,6 +27,23 @@ ActiveRecord::Schema.define(:version => 20120528091405) do
   end
 
   add_index "repos", ["full_name"], :name => "index_repos_on_full_name", :unique => true
+
+  create_table "taggings", :force => true do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
+    t.integer  "tagger_id"
+    t.string   "tagger_type"
+    t.string   "context",       :limit => 128
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
+
+  create_table "tags", :force => true do |t|
+    t.string "name"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "login"
