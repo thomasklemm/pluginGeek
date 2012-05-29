0a5ed9333a093a2cd2df541f33159086b5806660
Thomas Klemm <mail@tklemm.eu>
2012-05-29 14:45:56 +0200

    DB Columns Repo watchers and forks tranformed to integer

--- db/migrate/20120526080446_create_repos.rb
+++ db/migrate/20120526080446_create_repos.rb
@@ -1,15 +1,18 @@
 class CreateRepos < ActiveRecord::Migration
   def change
     create_table :repos do |t|
+      t.string :full_name
       t.string :owner
       t.string :name
-      t.string :watchers
-      t.string :forks
+      t.integer :watchers
+      t.integer :forks
       t.string :description
       t.string :github_url
       t.string :homepage_url
 
       t.timestamps
     end
+    
+    add_index :repos, :full_name, unique: true
   end
 end
