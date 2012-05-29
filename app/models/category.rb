716438126fdd07826937f0646374991681e3463c
Thomas Klemm <mail@tklemm.eu>
2012-05-29 19:21:21 +0200

    Category new attributes reflected in Category.update job

--- app/models/category.rb
+++ app/models/category.rb
@@ -7,22 +7,37 @@ class Category < ActiveRecord::Base
   extend FriendlyId
   friendly_id :name, use: :slugged
 
-  # Job to update Category table
+  # Default scope
+  default_scope order: 'watcher_count desc'
+
+
+  # Update Category table job
   def self.update
     tags = Repo.tag_counts_on(:categories)
     tags.each do |tag|
       category = self.find_or_initialize_by_name(tag.name)
+
+      # Popular Repos and All Repos (String)
+      repos = Repo.tagged_with(tag)
       
-      category[:count] = tag.count
-
-      repos = Repo.tagged_with(tag).limit(4)
-
+      # Popular Repos
       popular_repos = []
-      repos.each do |repo|
-        popular_repos << repo.full_name
-      end
+      repos[0..3].each { |repo| popular_repos << repo.full_name }
       category[:popular_repos] = popular_repos.join(", ")
 
+      # All Repos
+      # REVIEW: maybe only list those repos that are not in popular_repos
+      all_repos = []
+      repos.each { |repo| all_repos << repo.full_name }
+      category[:all_repos] = popular_repos.join(", ")
+
+      # Repo Count
+      category[:repo_count] = tag.count
+
+      # Watcher Count
+      category[:watcher_count] = repos.sum { |repo| repo.watchers }
+
+      # Save category
       category.save
     end
   end
