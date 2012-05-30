1f5079810dcf301573599cd855fd022d77b66375
Thomas Klemm <mail@tklemm.eu>
2012-05-30 10:44:18 +0200

    Add full seed data, including category_lists and category descriptions

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,8 @@
 
 # Repos
 repo_list = [
-  ['rails/rails', 'web application framework, high-level web application framework'],
-  ['sinatra/sinatra', 'web application framework, medium-level web application framework'],
+  ['rails/rails', 'web application framework'],
+  ['sinatra/sinatra', 'web application framework'],
   ['zurb/foundation', 'design framework'],
   ['twitter/bootstrap', 'design framework'],
   ['NoamB/sorcery', 'rails authentication, rails authorization'],
@@ -29,10 +29,11 @@ end
 
 # Categories
 category_list = [
+  ['web application framework', 'Build web applications with style.'],
   ['rails authentication', 'User Management for your Rails apps.'],
   ['design framework', 'Good foundation for your website styling.'],
   ['rails authorization', 'User Rights Management for your Rails apps.'],
-  ['development server', 'Local Development Server with automatic reload capabilites. Speeds up development.'],
+  ['development server', 'Local Development Server with automatic reload capabilites. Speeds up your development.'],
   ['rails tagging', 'Tagging functionality for your Rails apps.'],
   ['background jobs', 'Message queuing helps you allocating workload to seperate worker processes in the background. Used as synonyms: Background processing, message queues.'],
   ['active_record tagging', 'Tagging functionality for the ActiveRecord ORM (Rails Standard).']
@@ -42,6 +43,6 @@ category_list.each do |category|
   Category.create(name: category[0], description: category[1])
 end
 
-# Jobs
+# Run Jobs
 Repo.update_all_repos_from_github
 Category.update
