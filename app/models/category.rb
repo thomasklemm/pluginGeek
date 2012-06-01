3c5dfda37cfe36ffba9d3b188ab6c85d59315c9e
Thomas Klemm <mail@tklemm.eu>
2012-06-01 13:37:32 +0200

    Move category updater method

--- app/models/category.rb
+++ app/models/category.rb
@@ -14,38 +14,4 @@ class Category < ActiveRecord::Base
   # Validation
   validates :name, uniqueness: true
 
-  # Update Category table job
-  def self.update
-    tags = Repo.tag_counts_on(:categories)
-
-    tags.each do |tag|
-      category = self.find_or_initialize_by_name(tag.name)
-
-      # Popular Repos and All Repos (String)
-      repos = Repo.tagged_with(tag)
-
-      # Popular Repos
-      popular_repos = []
-      repos[0..3].each { |repo| popular_repos << repo.full_name }
-      category[:popular_repos] = popular_repos.join(", ")
-
-      # All Repos
-      # REVIEW: maybe only list those repos that are not in popular_repos
-      all_repos = []
-      repos.each { |repo| all_repos << repo.full_name }
-      category[:all_repos] = popular_repos.join(", ")
-
-      # Repo Count
-      category[:repo_count] = tag.count
-
-      # Watcher Count
-      category[:watcher_count] = repos.sum(&:watchers)
-      # category[:watcher_count] = repos.sum { |repo| repo.watchers }
-
-      # Save category
-      category.save
-    end
-  end
-
-
 end
--- app/models/updater.rb
+++ app/models/updater.rb
@@ -1,3 +1,47 @@
-class Updater < ActiveRecord::Base
-  # attr_accessible :title, :body
+class Updater
+
+  ###
+  #   Repos
+  ###
+
+  def update_repos_from_github
+
+  end
+
+  ###
+  #   Categories
+  ###
+
+  def update_categories_from_repos
+    tags = Repo.tag_counts_on(:categories)
+    tags.each { |tag| update_category_attributes(tag) }
+  end
+
+private
+
+  def update_category_attributes(tag)
+    # Find or initialize category
+    category = self.find_or_initialize_by_name(tag.name)
+
+    # Popular Repos and All Repos (String)
+    repos = Repo.tagged_with(tag)
+
+    # Popular Repos
+    category[:popular_repos] = repos[0..2].inject { |string, repo| string + ", " + repo.full_name }
+    # All Repos (excluding the ones already listed in popular_repos)
+    category[:all_repos] = repos[3..repos.length].inject { |string, repo| string + ", " + repo.full_name }
+
+    # Repo Count
+    category[:repo_count] = tag.count
+    # Watcher Count
+    category[:watcher_count] = repos.sum(&:watchers)
+    # category[:watcher_count] = repos.sum { |repo| repo.watchers }
+
+    # Save category
+    category.save
+  end
+
+
+  # TODO: Read up about method scoping (public / private / protected). What should be used here?
+
 end
