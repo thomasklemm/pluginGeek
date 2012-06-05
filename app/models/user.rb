c58e986737a3036cf472099cacd3976ae4b7a374
Thomas Klemm <mail@tklemm.eu>
2012-06-05 12:54:02 +0200

    User model attr_accessible and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ActiveRecord::Base
+
+  # Authenticates with sorcery!
   authenticates_with_sorcery!
-  # attr_accessible :title, :body
+
+  # Mass-assignment protection
+  attr_accessible :email, :password, :password_confirmation
+
+  # Validations
+  validates_confirmation_of :password
+  validates_presence_of :password, on: :create
+  validates_presence_of :email
+  validates_uniqueness_of :email
+
 end
