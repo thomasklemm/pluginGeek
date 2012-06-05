69f7dfbddfa966a2ab0000aba8fc2812ed3d69ee
Thomas Klemm <mail@tklemm.eu>
2012-06-05 13:04:51 +0200

    Add signup form and sessions controller create and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,17 @@
 
 class SessionsController < ApplicationController
-  def new
+  def create
+    user = login(params[:email], params[:password], params[:remember_me])
+    if user
+      redirect_back_or_to root_url, notice: 'Logged in!'
+    else
+      flash.now.alert = 'Email or password was invalid'
+      render :new
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to root_url, notice: 'Logged out!'
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,16 @@
 
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to root_url, notice: 'Signed up!'
+    else
+      flash.now.alert = 'Please try again.'
+      render :new
+    end
   end
 end
