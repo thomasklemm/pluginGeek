1a3612dd4e75c5c50a1f08715ababf9d3fcbfc04
Thomas Klemm <mail@tklemm.eu>
2012-06-08 12:38:30 +0200

    Repo partial

--- app/helpers/repos_helper.rb
+++ app/helpers/repos_helper.rb
@@ -1,2 +1,16 @@
 module ReposHelper
+
+  def activity_color(updated_at)
+    diff = Time.now - updated_at
+    klass = case diff
+            when 0..2.months                    then 'first'
+            when 2.months+1.second..6.months    then 'second'
+            when 6.months+1.second..12.months   then 'third'
+            when 12.months+1.second..18.months  then 'fourth'
+            when 18.months+1.second..24.months  then 'fifth'
+            else 'sixth'
+            end
+    klass
+  end
+
 end
