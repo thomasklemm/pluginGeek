2386eeff53b42841d071ce4d2dcf91063cc55cae
Thomas Klemm <mail@tklemm.eu>
2012-07-03 01:46:33 +0200

    Unicorn after_fork rule to avoid PG SSL errors

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,10 @@
 # Config for Unicorn app server
 
+# Loads Rails and App to enable super fast worker spawn times
+#  via https://github.com/blog/517-unicorn
+#  nescessary when using New Relic according to http://blog.railsonfire.com/2012/05/06/Unicorn-on-Heroku.html
+preload_app true
+
 # Amout of unicorn workers to spin up
 #  take a look at your memory usage via New Relic
 worker_processes 3
@@ -7,7 +12,9 @@ worker_processes 3
 # Restarts workers that hang for 30 seconds
 timeout 30
 
-# Loads Rails and App to enable super fast worker spawn times
-#  via https://github.com/blog/517-unicorn
-#  nescessary when using New Relic according to http://blog.railsonfire.com/2012/05/06/Unicorn-on-Heroku.html
-preload_app true
+# Avoiding SSL Errors
+#  via http://kuon.goyman.com/2012/heroku_unicorn/
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+  ActiveRecord::Base.establish_connection
+end
