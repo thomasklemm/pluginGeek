e93f87beec329c570b5e5d6d28483c659b22cc0f
Thomas Klemm <github@tklemm.eu>
2012-07-07 23:56:55 +0200

    Annotate Models and Routes.rb with Schema Info

--- test/unit/repo_test.rb
+++ test/unit/repo_test.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: repos
+#
+#  created_at        :datetime         not null
+#  description       :string(255)
+#  forks             :integer
+#  full_name         :string(255)
+#  github_updated_at :datetime
+#  github_url        :string(255)
+#  homepage_url      :string(255)
+#  id                :integer          not null, primary key
+#  knight_score      :integer
+#  name              :string(255)
+#  owner             :string(255)
+#  updated_at        :datetime         not null
+#  watchers          :integer
+#
+
 require 'test_helper'
 
 class RepoTest < ActiveSupport::TestCase
