7cc7a49a754f0c5b68c0d00579ab126b459c73f2
Thomas Klemm <github@tklemm.eu>
2012-07-12 18:53:09 +0200

    Use AWS CloudFront Asset Host in Production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Knight::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "//d2ishtm40wfhei.cloudfront.net"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( application_head.js application_body.js )
