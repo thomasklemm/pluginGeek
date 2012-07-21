f1bd7861642950b68a69dff02589016eb019932a
Thomas Klemm <github@tklemm.eu>
2012-07-21 10:15:29 +0200

    New Schema + Indexes

--- db/migrate/20120529101425_create_categories.rb
+++ db/migrate/20120529101425_create_categories.rb
@@ -3,15 +3,19 @@ class CreateCategories < ActiveRecord::Migration
     create_table :categories do |t|
       t.string :name
       t.string :slug
-      t.text :description
-      t.string :popular_repos
-      t.text :all_repos
       t.integer :repo_count
       t.integer :watcher_count
       t.integer :knight_score
+      t.text :short_description
+      t.text :description
+      t.text :md_description
+      t.string :popular_repos
+      t.text :all_repos
+
 
       t.timestamps
     end
     add_index :categories, :slug, unique: true
+    add_index :categories, :knight_score
   end
 end
