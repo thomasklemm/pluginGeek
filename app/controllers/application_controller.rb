e006da5502a96c092c035f609f206eb4a6856eec
Thomas Klemm <github@tklemm.eu>
2012-08-12 20:26:59 +0200

    CORS for Firefox Font Awesome

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,30 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   # Allow CORS for displaying Fonts from Cloudfront in Firefox
-  after_filter :set_cors
-  def set_cors
-    headers['Access-Control-Allow-Origin']  = '*'
-    headers['Access-Control-Allow-Methods'] = '*'
+  # Source: http://www.tsheffler.com/blog/?p=428
+  before_filter :cors_preflight_check
+  after_filter :cors_set_access_control_headers
+
+  # For all responses in this controller, return the CORS access control headers.
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+  # If this is a preflight OPTIONS request, then short-circuit the
+  # request, return only the necessary headers and return an empty
+  # text/plain.
+
+  def cors_preflight_check
+    if request.method == :options
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
+      headers['Access-Control-Max-Age'] = '1728000'
+      render :text => '', :content_type => 'text/plain'
+    end
   end
 
   def not_authenticated
