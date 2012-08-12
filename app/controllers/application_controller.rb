58456fbd2d1011ec327c3274a718990436add1fb
Thomas Klemm <github@tklemm.eu>
2012-08-12 22:35:58 +0200

    Cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   before_filter :cor
 
   def cor
-    headers["Access-Control-Allow-Origin"]  = "js-app-origin.com"
+    headers["Access-Control-Allow-Origin"]  = "knight.io"
     headers["Access-Control-Allow-Methods"] = %w{GET POST PUT DELETE}.join(",")
     headers["Access-Control-Allow-Headers"] = %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(",")
     head(:ok) if request.request_method == "OPTIONS"
