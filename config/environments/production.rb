9ec36853aa2a2e812f4329d17fc71ee1e8e16df5
Thomas Klemm <github@tklemm.eu>
2012-08-13 00:43:10 +0200

    Asset Host disabled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Knight::Application.configure do
   config.cache_store = :dalli_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = "http://d2ishtm40wfhei.cloudfront.net"
+  config.action_controller.asset_host = "ruby.knight.io"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( application_head.js application_body.js )
