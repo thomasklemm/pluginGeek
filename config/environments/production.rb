7fb85361b1d239e6e139e32829de2bd79f471db9
Thomas Klemm <github@tklemm.eu>
2012-08-13 02:56:42 +0200

    Middleware

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,5 +79,5 @@ Knight::Application.configure do
   # Use custom AccessControlHeader Middleware
   # insert before Rack::Cache and ActionDispatch::Static Middleware
   require 'rack-cache'
-  config.middleware.insert_before ActionDispatch::Static, AccessControlHeader
+  config.middleware.use AccessControlHeader
 end
