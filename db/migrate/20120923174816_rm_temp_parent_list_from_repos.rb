f3c3716770a83e5dd0134c3b70efbd6b6469521b
Thomas Klemm <github@tklemm.eu>
2012-09-23 20:54:01 +0200

    Rm files

--- db/migrate/20120923174816_rm_temp_parent_list_from_repos.rb
+++
@@ -1,9 +0,0 @@
-class RmTempParentListFromRepos < ActiveRecord::Migration
-  def up
-    remove_column :repos, :temp_parent_list
-  end
-
-  def down
-    add_column :repos, :temp_parent_list, :string
-  end
-end
