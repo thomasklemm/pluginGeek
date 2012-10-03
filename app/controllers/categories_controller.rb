39409916674c1729bc5740c927e6a29bc88b5076
Thomas Klemm <github@tklemm.eu>
2012-10-04 00:32:22 +0200

    Rm temp categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,7 +6,7 @@ class CategoriesController < ApplicationController
 
   # GET /categories
   def index
-    @categories = Category.find_all_by_language(language)
+    @categories = Category.find_all_by_language(params[:language])
     # Don't do such scalablity shit before it's nescessary.
     # Rather make happen that it becomes nescessary!
     # if !Rails.env.development? && !Rails.env.test?
