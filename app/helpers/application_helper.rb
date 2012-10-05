c5276fc5718d1be174fc69a2879974b2b0649d5d
Thomas Klemm <github@tklemm.eu>
2012-10-05 14:06:19 +0200

    Rework views in general

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,5 @@
 module ApplicationHelper
 
-  # Flash Messages Container Classes
-  def flash_message_container(type)
-    klass = case type
-            when :notice  then 'alert-box secondary'
-            when :success then 'alert-box secondary'
-            when :alert   then 'alert-box secondary'
-            when :error   then 'alert-box secondary'
-            else 'alert-box secondary'
-            end
-  end
-
   # A Greeting to all visiting Knights
   RUBY_NAMES = ['Ruby Warrior', 'Ruby Knight', 'Knight']
   def greeting_name
