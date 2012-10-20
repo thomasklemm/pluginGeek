723054a436baae9e5a2738bfca1b811994286079
Thomas Klemm <github@tklemm.eu>
2012-10-20 10:23:59 +0200

    Annotate models

--- app/models/categorization.rb
+++ app/models/categorization.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: categorizations
+#
+#  repo_id     :integer
+#  category_id :integer
+#  id          :integer          not null, primary key
+#
+
 class Categorization < ActiveRecord::Base
   belongs_to :repo,     touch: true
   belongs_to :category, touch: true
