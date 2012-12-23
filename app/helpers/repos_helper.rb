9526b6b4a1bcdcffe514fcae3510bfd31b682a9a
Thomas Klemm <github@tklemm.eu>
2012-12-23 19:22:20 +0100

    Styles for category#show displaying repos in a category

--- app/helpers/repos_helper.rb
+++ app/helpers/repos_helper.rb
@@ -3,7 +3,7 @@ module ReposHelper
   def activity_color(timestamp)
     diff = Time.zone.now - timestamp
     klass = case diff
-            when diff <= 2.months          then 'very-high'
+            when (-10000)..2.months        then 'very-high'
             when (2.months+1)..6.months    then 'high'
             when (6.months+1)..12.months   then 'medium'
             when (12.months+1)..24.months  then 'low'
