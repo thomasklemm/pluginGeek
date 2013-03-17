482d79771e119b7d1a1c3894de5f730fc3726ea3
Thomas Klemm <github@tklemm.eu>
2013-03-17 22:21:56 +0100

    Add field for spam prevention

--- app/controllers/mailers_controller.rb
+++ app/controllers/mailers_controller.rb
@@ -1,15 +1,12 @@
 class MailersController < ApplicationController
   # Send a feedback email from the field in the footer
   def feedback
-    if params[:honeypot].present?
-      redirect_to :back, alert: 'No spamming please.'
+    unless params[:honeypot].strip == '42'
+      flash.alert = "Please resubmit your message with answer 42."
+      return redirect_to root_path(feedback: params[:feedback])
     end
 
-    if params[:feedback_body].blank?
-      redirect_to :back, alert: 'Could not send an empty message'
-    end
-
-    mail = Mailer.feedback(params[:feedback_body], request)
+    mail = Mailer.feedback(params[:feedback], request)
     mail.deliver
 
     redirect_to :back, notice: 'Thomas just received your message. Thanks!'
