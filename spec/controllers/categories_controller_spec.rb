1d7d742913030ae19e722142cab066502d14d35a
Thomas Klemm <github@tklemm.eu>
2013-03-26 22:59:15 +0100

    Add specs for permitted category parameters

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -8,6 +8,71 @@ shared_context "categories" do
   let(:staff) { Fabricate(:user, staff: true) }
 end
 
+shared_context "category" do
+  let!(:category) { Fabricate(:category) }
+  let!(:repo)     { Fabricate(:repo, categories: [category]) }
+
+  let(:user)  { Fabricate(:user) }
+  let(:staff) { Fabricate(:user, staff: true) }
+end
+
+describe CategoriesController do
+  context "guest" do
+    it_should_behave_like "an authenticated controller", {
+      edit:   [:get, id: 1],
+      update: [:put, id: 1],
+      destroy: [:delete, id: 1]
+    }
+  end
+
+  describe "#category_params" do
+    let(:user)  { Fabricate(:user) }
+    let(:staff) { Fabricate(:user, staff: true) }
+
+    before do
+     subject.params[:category] = {
+        full_name: 'new_category_name',
+        description: 'new_category_description',
+        draft: false
+      }
+    end
+
+    context "staff" do
+      before  { sign_in staff }
+      let(:params) { controller.send(:category_params) }
+
+      it "permits description" do
+        expect(params[:description]).to be_present
+      end
+
+      it "permits full_name" do
+        expect(params[:full_name]).to be_present
+      end
+
+      it "permits draft" do
+        expect(params[:draft]).to_not be_nil
+      end
+    end
+
+    context "user" do
+      before  { sign_in user }
+      let(:params) { controller.send(:category_params) }
+
+      it "permits description" do
+        expect(params[:description]).to be_present
+      end
+
+      it "doesn't permit full_name" do
+        expect(params[:full_name]).to be_nil
+      end
+
+      it "doesn't permit draft" do
+        expect(params[:draft]).to be_nil
+      end
+    end
+  end
+end
+
 describe CategoriesController, "GET #index" do
   include_context "categories"
 
@@ -42,14 +107,6 @@ describe CategoriesController, "GET #index" do
     before { sign_in staff }
     include_examples "categories#index for guest, user and staff"
   end
-end
-
-shared_context "category" do
-  let!(:category) { Fabricate(:category) }
-  let!(:repo)     { Fabricate(:repo, categories: [category]) }
-
-  let(:user)  { Fabricate(:user) }
-  let(:staff) { Fabricate(:user, staff: true) }
 end
 
 describe CategoriesController, "GET #show" do
@@ -90,7 +147,7 @@ describe CategoriesController, "GET #show" do
   end
 end
 
-describe CategoriesController, "GET #edit", :focus do
+describe CategoriesController, "GET #edit" do
   include_context "category"
 
   shared_examples "categories#edit for user and staff" do
@@ -101,11 +158,6 @@ describe CategoriesController, "GET #edit", :focus do
     it { should_not set_the_flash }
   end
 
-  context "guest" do
-    before { get :edit, id: 1 }
-    it_should_behave_like "an authenticated controller action"
-  end
-
   context "user" do
     before { sign_in user }
     include_examples "categories#edit for user and staff"
@@ -114,6 +166,36 @@ describe CategoriesController, "GET #edit", :focus do
   context "staff" do
     before { sign_in staff }
     include_examples "categories#edit for user and staff"
+  end
+end
+
+describe CategoriesController, "PUT #update" do
+  include_context "category"
+
+  shared_examples "categories#update for user and staff" do
+    let(:valid_category_attributes) do
+      { full_name: 'new_category_name',
+      description: 'new_category_description',
+      draft: false }
+    end
+
+    before { put :update, id: category, category: valid_category_attributes }
+
+    it "redirects to the category" do
+      expect(response).to redirect_to(category)
+    end
+
+    it { should set_the_flash.to('Category has been updated.') }
+  end
+
+  context "user" do
+    before { sign_in user }
+    include_examples "categories#update for user and staff"
+  end
+
+  context "staff" do
+    before { sign_in staff }
+    include_examples "categories#update for user and staff"
   end
 end
 
--- spec/controllers/repos_controller_spec.rb
+++ spec/controllers/repos_controller_spec.rb
@@ -1,8 +1,4 @@
 require 'spec_helper'
 
 describe ReposController do
-  it_should_behave_like "an authenticated controller", {
-    edit: [:get, id: 1],
-    update: [:put, id: 1]
-  }
 end
