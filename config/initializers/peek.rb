a70f637a3dc8adde3caa9ceb02175eb1b2ce2ecc
Thomas Klemm <github@tklemm.eu>
2013-03-29 14:46:59 +0100

    Change peek widget order

--- config/initializers/peek.rb
+++ config/initializers/peek.rb
@@ -2,7 +2,7 @@ if Rails.env.development?
   Peek.into Peek::Views::Git
 end
 
+Peek.into Peek::Views::PerformanceBar
 Peek.into Peek::Views::PG
 Peek.into Peek::Views::Dalli
-Peek.into Peek::Views::PerformanceBar
 
