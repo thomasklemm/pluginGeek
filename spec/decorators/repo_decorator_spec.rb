a5e6e3d24ea66821cb316177ff77d3ed3590cc9f
Thomas Klemm <github@tklemm.eu>
2013-03-31 21:16:53 +0200

    Add specs for RepoDecorator

--- spec/decorators/repo_decorator_spec.rb
+++ spec/decorators/repo_decorator_spec.rb
@@ -5,4 +5,45 @@ describe RepoDecorator do
     repo = Fabricate.build(:repo)
     repo.decorate
   end
+
+  it "decorates associated categories" do
+    expect(repo.categories).to be_decorated
+  end
+
+  it "decorates associated parents_and_children" do
+    expect(repo.parents_and_children).to be_decorated
+  end
+
+  describe "#owner" do
+    it "returns the owner when given" do
+      repo.owner = "owner"
+      expect(repo.owner).to eq("owner")
+    end
+
+    it "splits the full_name and returns the owner part when owner not given" do
+      repo.owner = ""
+      repo.full_name = "owner/name"
+      expect(repo.owner).to eq("owner")
+    end
+  end
+
+  describe "#name" do
+    it "returns the name when given" do
+      repo.name = "name"
+      expect(repo.name).to eq("name")
+    end
+
+    it "splits the full_name and returns the name part when name not given" do
+      repo.name = ""
+      repo.full_name = "owner/name"
+      expect(repo.name).to eq("name")
+    end
+  end
+
+  describe "#description"
+  describe "#github_description"
+  describe "#homepage_url"
+  describe "#timestamp"
+  describe "#written_timestamp"
+  describe "#activity_class"
 end
