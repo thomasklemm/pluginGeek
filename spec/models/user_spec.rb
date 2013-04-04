a7516b36daa34b4b0c34a9ad8cb844d8d28d63a3
Thomas Klemm <github@tklemm.eu>
2013-04-04 08:37:13 +0200

    Add specs for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -53,15 +53,79 @@ describe User do
     let(:omniauth) { OmniAuth.config.mock_auth[:github] }
 
     context "with an already existing user" do
-      it "finds the user by the authentication"
-      it "updates the user's fields from Github"
-      it "returns the user"
+      let!(:existing_user)  { Fabricate(:user) }
+      let!(:authentication) { Fabricate(:authentication, user: existing_user, provider: :github, uid: 1100176) }
+      let(:user) { User.find_or_create_user_from_github(omniauth) }
+
+      it "finds the user by the authentication" do
+        expect { user }.to_not change { User.count }
+        expect(user.authentications.first).to eq(authentication)
+      end
+
+      it "updates the user's fields from the auth hash" do
+        info = omniauth.info
+        raw_info = omniauth.extra.raw_info
+
+        expect(user.login).to eq(info.nickname)
+        expect(user.name).to  eq(info.name)
+        expect(user.email).to eq(info.email)
+        expect(user.avatar_url).to eq(info.image)
+        expect(user.company).to    eq(raw_info.company)
+        expect(user.location).to   eq(raw_info.location)
+        expect(user.followers).to  eq(raw_info.followers)
+
+        expect(user.login).to be_present
+        expect(user.name).to  be_present
+        expect(user.email).to be_present
+        expect(user.avatar_url).to be_present
+        expect(user.company).to    be_present
+        expect(user.location).to   be_present
+        expect(user.followers).to  be_present
+      end
+
+      it "returns the user" do
+        expect(user).to be_a(User)
+      end
     end
 
     context "with a new user" do
-      it "creates the new user"
-      it "creates the authentication"
-      it "returns the user"
+      let(:user) { User.find_or_create_user_from_github(omniauth) }
+
+      it "assigns the user's fields from the auth hash" do
+        info = omniauth.info
+        raw_info = omniauth.extra.raw_info
+
+        expect(user.login).to eq(info.nickname)
+        expect(user.name).to  eq(info.name)
+        expect(user.email).to eq(info.email)
+        expect(user.avatar_url).to eq(info.image)
+        expect(user.company).to    eq(raw_info.company)
+        expect(user.location).to   eq(raw_info.location)
+        expect(user.followers).to  eq(raw_info.followers)
+
+        expect(user.login).to be_present
+        expect(user.name).to  be_present
+        expect(user.email).to be_present
+        expect(user.avatar_url).to be_present
+        expect(user.company).to    be_present
+        expect(user.location).to   be_present
+        expect(user.followers).to  be_present
+      end
+
+      it "saves the the new user in the database" do
+        expect { user }.to change { User.count }.by(1)
+        expect(user).to be_persisted
+      end
+
+      it "creates the authentication" do
+        expect { user }.to change { Authentication.count }.by(1)
+        expect(user.authentications).to be_present
+        expect(user.authentications.first).to be_persisted
+      end
+
+      it "returns the user" do
+        expect(user).to be_a(User)
+      end
     end
   end
 end
