bd60655c5ed87ea25740300d2231a189451342e8
Thomas Klemm <github@tklemm.eu>
2013-04-16 11:16:20 +0200

    Rework categories features specs

--- spec/features/categories_spec.rb
+++ spec/features/categories_spec.rb
@@ -11,154 +11,95 @@ def sign_out
   logout(:user)
 end
 
-feature "Categories#index" do
+shared_context "category" do
+  let!(:language) { Fabricate(:language, name: "Ruby") }
+  let!(:category) { Fabricate(:category, languages: [language], repos: [repo]) }
+  let!(:repo)     { Fabricate(:repo) }
+
   let(:user)  { Fabricate(:user) }
   let(:staff) { Fabricate(:user, staff: true) }
+end
 
-  let(:language) { Fabricate(:language, name: 'Ruby') }
-  let(:category) { Fabricate(:category) }
-  let(:repo)     { Fabricate(:repo) }
+describe Category, "list categories" do
+  include_context "category"
 
-  shared_examples "list categories" do
-    background do
-      category.languages << language
-      category.repos << repo
-      category.save
-    end
+  it "lists categories" do
+    visit categories_path(language.slug)
+    expect(current_path).to eq("/#{ language.slug }")
 
-    scenario "list categories" do
-      visit categories_path(language.slug)
-      expect(current_path).to eq("/#{language.slug}")
-
-      expect(page).to have_content(category.full_name)
-      expect(page).to have_content(repo.name)
-      expect(page).to have_content(language.name)
-    end
-  end
-
-  describe "staff access" do
-    before { sign_in staff }
-    include_examples "list categories"
-  end
-
-  describe "user access" do
-    before { sign_in user }
-    include_examples "list categories"
-  end
-
-  describe "guest access" do
-    include_examples "list categories"
+    expect(page).to have_content(category.full_name)
+    expect(page).to have_content(repo.name)
+    expect(page).to have_content(language.name)
   end
 end
 
-feature "Categories#show" do
-  let(:user)  { Fabricate(:user) }
-  let(:staff) { Fabricate(:user, staff: true) }
+describe Category, "show category" do
+  include_context "category"
+  let!(:link)     { Fabricate(:link, categories: [category]) }
+  let!(:link_via_repo)    { Fabricate(:link, repos: [repo]) }
+  let!(:similar_category) { Fabricate(:category, related_categories: [category]) }
 
-  let(:language) { Fabricate(:language, name: 'Ruby') }
-  let(:category) { Fabricate(:category) }
-  let(:repo)     { Fabricate(:repo) }
-  let(:link)     { Fabricate(:link) }
-  let(:link_via_repo)    { Fabricate(:link) }
-  let(:similar_category) { Fabricate(:category) }
+  it "shows the category" do
+    path = category_path(category)
+    visit path
+    expect(current_path).to eq(path)
 
-  shared_examples "render category" do
-    background do
-      category.languages << language
-      category.repos << repo
-      category.related_categories << similar_category
-      category.links << link
-      repo.links << link_via_repo
-    end
+    expect(page).to have_content(category.full_name)
+    expect(page).to have_content(language.name)
 
-    scenario "render category" do
-      path = category_path(category)
-      visit path
-      expect(current_path).to eq(path)
+    expect(page).to have_content(repo.full_name)
+    expect(page).to have_content(similar_category.full_name)
 
-      expect(page).to have_content(category.full_name)
-      expect(page).to have_content(language.name)
-
-      expect(page).to have_content(repo.full_name)
-      expect(page).to have_content(similar_category.full_name)
-
-      expect(page).to have_content(link.title)
-      expect(page).to have_content(link_via_repo.title)
-    end
-  end
-
-  describe "staff access" do
-    before { sign_in staff }
-    include_examples "render category"
-  end
-
-  describe "user access" do
-    before { sign_in user }
-    include_examples "render category"
-  end
-
-  describe "guest access" do
-    include_examples "render category"
+    expect(page).to have_content(link.title)
+    expect(page).to have_content(link_via_repo.title)
   end
 end
 
-feature "Categories#edit and categories#update" do
-  let(:user)  { Fabricate(:user) }
-  let(:staff) { Fabricate(:user, staff: true) }
+describe Category, "edit category" do
+  include_context "category"
 
-  let(:language) { Fabricate(:language, name: 'Ruby') }
-  let(:category) { Fabricate(:category) }
-  let(:repo)     { Fabricate(:repo) }
-  let(:link)     { Fabricate(:link) }
-  let(:link_via_repo)    { Fabricate(:link) }
-  let(:similar_category) { Fabricate(:category) }
+  it "updates the category" do
+    sign_in user
 
-  background do
-    category.languages << language
-    category.repos << repo
-    category.related_categories << similar_category
-    category.links << link
-    repo.links << link_via_repo
-  end
+    visit category_path(category)
+    click_on "Edit"
+    expect(current_path).to eq(edit_category_path(category))
 
-  shared_examples "render form" do
-    scenario "render form" do
-      path = edit_category_path(category)
-      visit path
-      expect(current_path).to eq(path)
-    end
-  end
+    fill_in "describe", with: "some new description"
+    click_on "Update"
 
-  describe "staff access" do
-    before { sign_in staff }
-    include_examples "render form"
-  end
-
-  describe "user access" do
-    before { sign_in user }
-    include_examples "render form"
-  end
-
-  describe "guest access" do
-    scenario "forbidden request" do
-      visit edit_category_path(category)
-
-      expect(current_path).to eq(login_path)
-      expect(page).to have_content(/login with github/i)
-    end
+    expect(current_path).to eq(category_path(category))
+    expect(page).to have_content(/Category has been updated/)
+    expect(page).to have_content("some new description")
   end
 end
 
-feature "Refresh category" do
-  let(:category) { Fabricate(:category) }
-  let(:staff) { Fabricate(:user, staff: true) }
-  before { sign_in staff }
+describe Category, "refresh category" do
+  include_context "category"
 
-  scenario "expire category with click on refresh" do
+  it "refreshes the category" do
+    sign_in staff
+
     visit category_path(category)
     click_on "Refresh"
 
     expect(current_path).to eq(category_path(category))
     expect(page).to have_content(/Category has been refreshed/)
+  end
+end
+
+describe Category, "destroy category" do
+  include_context "category"
+
+  it "destroys the category" do
+    sign_in staff
+
+    visit edit_category_path(category)
+    expect(current_path).to eq(edit_category_path(category))
+
+    click_on "Destroy"
+
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content(/Category has been destroyed/)
   end
 end
--- spec/features/load_testing_spec.rb
+++ spec/features/load_testing_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
-feature "Authorize Blitz.io load testing" do
-  scenario "responds to authorization url with code" do
+describe "Blitz.io load testing authorization" do
+  it "authorizes load testing" do
     path = "/mu-a4ca81c6-8526fed8-0bc25966-0b2cc605"
     visit path
-    expect(current_path).to eq(path)
 
+    expect(current_path).to eq(path)
     expect(page.body).to eq("42")
   end
 end
