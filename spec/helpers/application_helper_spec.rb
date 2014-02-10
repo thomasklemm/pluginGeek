8252855ec839ebdc861ec564a4b5c2926466501f
Thomas Klemm <github@tklemm.eu>
2014-02-10 17:29:30 +0100

    Fix helpers and helper specs

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,18 +1,6 @@
 require 'spec_helper'
 
 describe ApplicationHelper do
-  describe "#response_publicly_cached?" do
-    it "returns true when response will be publicly cached" do
-      response.cache_control[:public] = true
-      expect(helper.response_publicly_cached?).to be_true
-    end
-
-    it "returns false when response won't be publicly cached" do
-      response.cache_control[:public] = false
-      expect(helper.response_publicly_cached?).to be_false
-    end
-  end
-
   describe "#dns_prefetch_tag(url)" do
     it "returns a valid DNS prefetch tag" do
       expected_tag = "<link rel='dns-prefetch' href='http://www.example.com'>"
@@ -22,13 +10,25 @@ describe ApplicationHelper do
 
   describe "#icon_tag(:type)" do
     it "returns a webfont icon tag" do
-      expect(helper.icon_tag(:check)).to match(/<i class='icon-check'><\/i>/)
+      expect(helper.icon_tag(:check)).to match(/<i class='fa fa-check'><\/i>/)
     end
   end
 
   describe "#markdown(text)" do
     it "renders markdown text" do
       expect(helper.markdown('**Hello world**')).to match('<p><strong>Hello world</strong></p>')
+    end
+  end
+
+  describe "#response_publicly_cached?" do
+    it "returns true when response will be publicly cached" do
+      response.cache_control[:public] = true
+      expect(helper.response_publicly_cached?).to be_true
+    end
+
+    it "returns false when response won't be publicly cached" do
+      response.cache_control[:public] = false
+      expect(helper.response_publicly_cached?).to be_false
     end
   end
 end
