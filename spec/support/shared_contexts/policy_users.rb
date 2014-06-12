2596057c0764b9073978461144b589391cb05aa6
Thomas Klemm <github@tklemm.eu>
2014-06-12 18:34:16 +0200

    Remove empty files

--- spec/support/shared_contexts/policy_users.rb
+++
@@ -1,7 +0,0 @@
-# Pundit policy users
-#
-shared_context "policy_users" do
-  let(:moderator) { Fabricate.build(:user, moderator: true) }
-  let(:user)      { Fabricate.build(:user) }
-  let(:guest)     { nil }
-end
