95f1d58e183eab952b6ba5c2ca3a6811c18f5bbd
Thomas Klemm <github@tklemm.eu>
2014-07-25 10:18:15 +0200

    Global platform specs

--- spec/models/platform/global_spec.rb
+++ spec/models/platform/global_spec.rb
@@ -4,20 +4,36 @@ describe Platform::Global do
   subject(:global) { described_class.instance }
 
   describe '.global_id?' do
-    it 'returns true if the global id is given as a string'
-    it 'returns true if the global id is given as a symbol'
-    it 'returns false if another identifier is given'
+    it 'returns true if the global id is given as a string' do
+      expect(described_class.global_id?('global')).to be_truthy
+    end
+
+    it 'returns true if the global id is given as a symbol' do
+      expect(described_class.global_id?(:global)).to be_truthy
+    end
+
+    it 'returns false if another identifier is given' do
+      expect(described_class.global_id?('ruby')).to be_falsey
+    end
   end
 
   describe '#id' do
-    it 'returns the global id'
+    it 'returns the global id' do
+      expect(global.id).to eq 'global'
+    end
   end
 
   describe '#categories' do
-    it 'returns a relation for all categories'
+    it 'returns a relation for all categories' do
+      expect(Category).to receive(:all).and_return('all categories')
+      expect(global.categories).to eq 'all categories'
+    end
   end
 
   describe '#categories_count' do
-    it 'returns a count including all categories'
+    it 'returns a count including all categories' do
+      expect(Category).to receive(:count).and_return(42)
+      expect(global.categories_count).to eq 42
+    end
   end
 end
